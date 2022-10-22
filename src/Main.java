e4947a54b01e2bcfe886578e07dfdd8a595d64e6
imran <ih060899@gmail.com>
2022-10-22 20:16:34 +0600

    MortgageCalculator

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println(Thread.activeCount());
+        System.out.println(Runtime.getRuntime().availableProcessors());
         System.out.println("Hello world!");
     }
 }
---
+++ src/streams/StreamsDemo.java
@@ -0,0 +1,6 @@
+package streams;
+
+public class StreamsDemo {
+  public static void show() {
+  }
+}
